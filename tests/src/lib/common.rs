4e62acb18339107f06e9fb0f01e30ac3981fcb34
Aaron Kutch <aaronkutch@att.net>
2023-08-22 16:39:21 -0500

    update to Onomy v1.1.2

--- tests/src/lib/common.rs
+++ tests/src/lib/common.rs
@@ -19,5 +19,5 @@ pub fn consumer_directory() -> String {
 const DOWNLOAD_ONOMYD: &str = r#"ADD https://github.com/onomyprotocol/onomy/releases/download/$DAEMON_VERSION/onomyd $DAEMON_HOME/cosmovisor/genesis/$DAEMON_VERSION/bin/onomyd"#;
 
 pub fn dockerfile_onomyd() -> String {
-    onomy_std_cosmos_daemon_with_arbitrary("onomyd", ".onomy", "v1.1.1", DOWNLOAD_ONOMYD)
+    onomy_std_cosmos_daemon_with_arbitrary("onomyd", ".onomy", "v1.1.2", DOWNLOAD_ONOMYD)
 }
